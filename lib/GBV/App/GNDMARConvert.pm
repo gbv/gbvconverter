3140b1cd1bce437d954a60086290124242dbbf21
Jakob Voss <voss@gbv.de>
2015-03-09 15:09:48 +0100

    split packages

--- lib/GBV/App/GNDMARConvert.pm
+++ lib/GBV/App/GNDMARConvert.pm
@@ -4,11 +4,14 @@ use v5.14.1;
 our $VERSION="0.0.0";
 our $NAME="gndmarconvert";
 
-use parent 'Plack::Component';
+use parent 'Plack::App::Converter';
 
 use Plack::Builder;
-use Plack::Request;
-use JSON qw(to_json);
+
+## TODO: more conversions
+use File::Temp qw(tempfile);
+use GBV::App::GNDMARConvert::Mapping;
+###
 
 my $FORMATS = {
     marcxml => { 
@@ -16,15 +19,36 @@ my $FORMATS = {
         title => 'MARCXML',
         docs => 'http://www.loc.gov/standards/marcxml/',
         extension => 'xml',
-        targets => [qw(json)],
+        targets => {
+            json => \&marcxml2json,
+            'json-ld' => \&marcxml2jsonld,
+        }
     },
     json => {
         type => 'application/json',
         title => 'JSON',
         extension => 'json',
-    }
+    },
+    'json-ld' => {
+        type => 'application/ld+json',
+        title => 'JSON-LD',
+        extension => 'json'
+    },
 };
-$FORMATS->{$_}->{name} = $_ for keys %$FORMATS;
+
+sub marcxml2json {
+    my ($input) = @_; # Plack::Request::Upload
+    my $filename = $input->tempname;
+    my $json = marc_file_to_json($filename);
+    # $fh = tempfile();
+    return [ JSON->new->pretty->utf8->encode($json) ];
+}
+
+sub marcxml2jsonld {
+    my ($input) = @_; # Plack::Request::Upload
+    # ...
+    return [ "" ]
+}
 
 sub prepare_app {
     my ($self) = @_;
@@ -54,111 +78,10 @@ sub prepare_app {
             pass_through => 1, root => "/etc/$NAME/htdocs";
         enable 'Static', path => qr{\.(html|js|ico|css|png)},
             pass_through => 1, root => './htdocs';
-        enable 'ContentLength';
-        builder {
-            mount '/formats' => sub { $self->formats(@_) };
-            mount '/convert' => sub { $self->convert(@_) };
-            mount '/' => sub { [404,[],['Not Found']] };
-        };
+        Plack::App::Converter->new( 
+            formats => $FORMATS 
+        );
     };
 }    
-
-sub convert {
-    my ($self, $env) = @_;
-    my $req = Plack::Request->new($env);
-    if ($req->method ne 'POST') {
-        return [ 405, [], ['Method Not Allowed'] ];
-    }
-
-    my $input   = $req->query_parameters->{'input'} // 'upload';
-    my $uploads = $req->uploads;
-    my ($file)  = values %$uploads;
-
-    if ( $input ne 'upload' ) {
-        return [ 400, [], ['Only input=upload supported'] ];
-    } elsif ( !%$uploads or !$file ) {
-        return [ 400, [], ['Missing upload file'] ];
-    } elsif ( $uploads->keys > 1 ) {
-        return [ 400, [], ['Too many upload files'] ];
-    }
-
-    my %format = (
-        input => $self->guess_format(
-            name => $req->parameters->{'inputformat'},
-            type => $file->content_type,
-            file => $file->filename
-        ),
-        output => $self->guess_format( 
-            name => $req->parameters->{'outputformat'},
-            type => $req->header('Accept') 
-        )
-    );
-
-    foreach ( qw(input output) ) {
-        if ( !$format{$_} ) {
-            return [ 400, [], ["Missing ${_}format"] ];
-        } 
-        $format{$_} = $FORMATS->{$format{$_}};
-        if (!$format{$_}) {
-            return [ 400, [], ["Unknown ${_}format"] ];
-        }
-    }
-
-    if ( $format{input}->{name} ne 'marcxml' ) {
-        return [ 400, [], ['Only inputformat=marcxml supported'] ];
-    }
-
-    if ( $format{output}->{name} ne 'json' ) {
-        return [ 400, [], ['Only outputformat=json supported'] ];
-    }
-
-    my @headers;
-
-    my $outputfile = $file->filename;
-    my $ext = $format{output}->{extension};
-    $outputfile = $file->filename.'.'.$ext unless $outputfile =~ s/\.([^.]+)$/.$ext/;
-    push @headers, 'Content-Disposition' => "attachment; filename=\"$outputfile\"";
- 
-    push @headers, 'Content-Type' => $format{output}->{type};
-
-    my $result = $self->perform_convert( $file, $format{input}, $format{output} );
-    return [ 200, \@headers, $result ];
-}
-
-## TODO: more conversions
-use GBV::App::GNDMARConvert::Mapping;
-sub perform_convert {
-    my ($self, $file, $input, $output) = @_;
-    my $filename = $file->tempname;
-    my $json = marc_file_to_json($filename);
-    return [ JSON->new->pretty->utf8->encode($json) ];
-}
-
-sub guess_format {
-    my ($self, %properties) = @_;
-    
-    return $properties{name} if defined $properties{name};
-
-    my @formats = grep { $FORMATS->{$_}->{type} eq $properties{type} }
-                  keys %$FORMATS;
-
-    if (@formats != 1 and $properties{file} and $properties{file} =~ /\.([^.]+)$/) {
-        my $extension = $1 // '';
-        @formats = grep { $FORMATS->{$_}->{extension} eq $extension } keys %$FORMATS;
-    }
-
-    return @formats == 1 ? $formats[0] : undef;
-}
-
-sub formats {
-    [ 200, [ 'Content-Type' => 'application/json' ], 
-        [ JSON->new->pretty->encode($FORMATS) ] 
-    ];
-}
-
-sub call {
-    my ($self, $env) = @_;
-    $self->{app}->($env);
-}
 
 1;
---
+++ lib/Plack/App/Converter.pm
@@ -0,0 +1,143 @@
+package Plack::App::Converter;
+use v5.14.1;
+
+use parent 'Plack::Component';
+
+use Plack::Builder;
+use Plack::Request;
+use Plack::Util::Accessor qw(formats);
+use JSON;
+use Storable qw(dclone);
+
+sub prepare_app {
+    my ($self) = @_;
+    return if $self->{app};
+
+    $self->{conversions} = { };
+    foreach (keys %{$self->{formats}}) {
+        $self->{formats}->{$_}->{name} = $_;
+        if ( my $target = delete $self->{formats}->{$_}->{targets} ) {
+            $self->{conversions}->{$_} = $target;
+            $self->{formats}->{$_}->{targets} = keys [ %$target ];
+        }
+    }
+
+    # List of formats in JSON
+    my $FORMATS = JSON->new->pretty->encode($self->{formats});
+
+    $self->{app} = builder {
+        enable 'ContentLength';
+        builder {
+            mount '/formats' => sub {
+                [ 200, [ 'Content-Type' => 'application/json' ], [ $FORMATS ] ];
+            };
+            mount '/convert' => sub { $self->convert(@_) };
+            mount '/' => sub { [404,[],['Not Found']] };
+        };
+    };
+}
+
+sub convert {
+    my ($self, $env) = @_;
+
+    if ($env->{REQUEST_METHOD} ne 'POST') {
+        return [ 405, [], ['Method Not Allowed'] ];
+    }
+    
+    my $req = Plack::Request->new($env);
+    my $input   = $req->query_parameters->{'input'} // 'upload';
+    my $uploads = $req->uploads;
+    my ($file)  = values %$uploads;
+
+    if ( $input ne 'upload' ) {
+        return [ 400, [], ['Only input=upload supported'] ];
+    } elsif ( !%$uploads or !$file ) {
+        return [ 400, [], ['Missing upload file'] ];
+    } elsif ( $uploads->keys > 1 ) {
+        # TODO: some formats may support multiple files
+        return [ 400, [], ['Too many upload files'] ];
+    }
+
+    my %format = (
+        input => $self->guess_format(
+            name => $req->parameters->{from},
+            type => $file->content_type,
+            file => $file->filename
+        ),
+        output => $self->guess_format( 
+            name => $req->parameters->{to},
+            type => $req->header('Accept') 
+        )
+    );
+
+    foreach ( qw(input output) ) {
+        if ( !$format{$_} ) {
+            return [ 400, [], ["Missing ${_}format"] ];
+        } 
+        $format{$_} = $self->{formats}->{$format{$_}};
+        if (!$format{$_}) {
+            return [ 400, [], ["Unknown ${_}format"] ];
+        }
+    }
+
+    my $from = $format{input}->{name};
+    my $to   = $format{output}->{name};
+
+    print "FROM: $from\n";
+    if ( !$self->{conversions}->{$from} ) {
+        return [ 400, [], ["Invalid input format"] ];
+    }
+    my $conversion = $self->{conversions}->{$from}->{$to};
+    if (!$conversion) {
+        return [ 400, [], ["Unsupported output format"] ];
+    }
+
+    # construct response
+    my @headers;
+    my $outputfile = $file->filename;
+    my $ext = $format{output}->{extension};
+    $outputfile = $file->filename.'.'.$ext unless $outputfile =~ s/\.([^.]+)$/.$ext/;
+    push @headers, 'Content-Disposition' => "attachment; filename=\"$outputfile\"";
+ 
+    push @headers, 'Content-Type' => $format{output}->{type};
+    return [ 200, \@headers, $conversion->( $file ) ];
+}
+
+sub guess_format {
+    my ($self, %properties) = @_;
+    
+    return $properties{name} if defined $properties{name};
+
+    my @formats = grep { $self->{formats}->{$_}->{type} eq $properties{type} }
+                   keys %{$self->{formats}};
+
+    if (@formats != 1 and $properties{file} and $properties{file} =~ /\.([^.]+)$/) {
+        my $extension = $1 // '';
+        @formats = grep { $self->{formats}->{$_}->{extension} eq $extension } 
+                   keys %{$self->{formats}};
+    }
+
+    return @formats == 1 ? $formats[0] : undef;
+}
+
+sub call {
+    my ($self, $env) = @_;
+    $self->{app}->($env);
+}
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+  my $app = Plack::App::Convert->new( formats => $formats );
+
+=head1 METHODS
+
+=head2 convert
+
+=head2 guess_format
+
+=head2 call
+
+=cut
